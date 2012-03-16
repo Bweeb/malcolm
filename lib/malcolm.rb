c90aabea6e83716f84c554fe620e4878c3f94ce7
tfwright <tfwright@gmail.com>
2012-03-16 11:59:13 -0500

    Removed registration calls

--- lib/malcolm.rb
+++ lib/malcolm.rb
@@ -3,11 +3,4 @@ require 'faraday'
 module Malcolm
   autoload :SOAPBuilder,      'malcolm/request/soap_builder'
   autoload :FantasticoParser, 'malcolm/response/fantastico_parser'
-
-  Faraday.register_middleware :request,
-    :soap => SOAPBuilder
-    
-  Faraday.register_middleware :response,
-    :fantastico => FantasticoParser
-
 end
