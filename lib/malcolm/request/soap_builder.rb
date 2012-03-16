6f2888f23bf05c8aa1d60a5ecc4d3c54c6ce5ea8
tfwright <tfwright@gmail.com>
2012-03-16 09:58:36 -0500

    Fixed XML conversion, Added some tests

--- lib/malcolm/request/soap_builder.rb
+++ lib/malcolm/request/soap_builder.rb
@@ -2,7 +2,7 @@ module Malcolm
   # Request middleware that converts params to XML and wraps them in a SOAP envelope.
   #See http://www.w3.org/TR/2007/REC-soap12-part0-20070427/
   class SOAPBuilder < Faraday::Middleware
-    dependency 'xml-simple'
+    dependency 'xmlsimple'
     
     # Assumes request body is a hash of key-value pairs    
     def call(env)
@@ -14,8 +14,9 @@ module Malcolm
     
     # Builds an XML document around request data
     def wrap(data)
+      raise "Request body already serialized.  Please make sure it is a hash before applying SOAP Builder." unless data.is_a?(Hash)
       "<?xml version=\"1.0\" encoding=\"UTF-8\"?><env:Envelope xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\"><env:Body>".tap do |soap_envelope|
-        soap_envelope << XmlSimple.xml_out(data)
+        soap_envelope << XmlSimple.xml_out(data, :keep_root => true) if data
         soap_envelope << "</env:Body></env:Envelope>"
       end
     end
