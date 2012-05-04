8c25515bf88b11a32c9378f76cc405b5f21e3dcd
tfwright <tfwright@gmail.com>
2012-05-04 15:11:19 -0500

    Made body handling flexible enough to handle strings

--- spec/malcolm/request/soap_builder_spec.rb
+++ spec/malcolm/request/soap_builder_spec.rb
@@ -18,10 +18,12 @@ describe Malcolm::SOAPBuilder do
     xml.should include %{<test>data}
   end
   
-  it "raises if body isn't a hash" do
+  it "directly inserts string data into hash" do
     middleware = described_class.new(lambda{|env| env})
-    env = {:body => "test=data", :request_headers => Faraday::Utils::Headers.new}
-    expect { middleware.call(env) }.to raise_error
+    env = {:body => "<test>data</test>", :request_headers => Faraday::Utils::Headers.new}
+    result = middleware.call(env)
+    xml = result[:body]
+    xml.should include %{<test>data</test>}
   end
   
   it "ignores empty data" do
